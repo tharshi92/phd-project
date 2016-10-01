6023f718ee54a3a5723d2bb24f278a0ecc5f6be5
Tharshi Sri <tsrikann@physics.utoronto.ca>
2016-10-01 14:02:14 -0400

    updated prepNetwork

--- animus/prepNetwork.py
+++ animus/prepNetwork.py
@@ -17,30 +17,43 @@ for metadata in metadatum:
     else:
         field = np.load(title + '.npy').reshape((len(state), 1))
 
-mu_x = np.mean(data, axis=0)
-s_x = np.std(data, axis=0, ddof=1)
-mu_y = np.mean(target, axis=0)
-s_y = np.std(target, axis=0)
-scale_params = [mu_x, s_x, mu_y]
+mu_x = np.mean(state, axis=0)
+s_x = np.std(state, axis=0, ddof=1)
+mu_y = np.mean(field, axis=0)
+s_y = np.std(field, axis=0, ddof=1)
+scale_params = [mu_x, s_x, mu_y, s_y]
+
+x = (state - mu_x)/s_x
+y = (field - mu_y)/s_y
+
+testingYear = 2007
+leap = 0
+flag = (testingYear - 2006 + 2) % 4
+if flag == 0:
+	leap = 1
+numLeap = 0
+for i in range(testingYear - 2006):
+	if (i + 2) % 4:
+		numLeap += 1
+num = (testingYear - 2006) - numLeap
+start = 365 * num + 366 * numLeap
+end = start + 365 * (1 - leap) + 366 * leap
+
+testingData = np.hstack((x[start:end, :], y[start:end, :]))
+trainingData = np.hstack(np.ystack(x[:start, :], x[:end, :]), \
+	np.ystack(y[:start, :], y[:end, :]))
 
 # shuffle x and y training arrays
 seed = int(27)
-new_order = list(range(len(data)))
+new_order = list(range(len(trainingData)))
 random.seed(seed)
 random.shuffle(new_order)
 
-x = (data[new_order, :] - mu_x)/s_x
-y = target[new_order, :]
-x_test = (data_test - mu_x)/s_x
-y_test = target_test
+trainingData = trainingData[new_order, :]
 
-np.save('x', x)
-np.save('y', y)   
-np.save('xt', x_test)
-np.save('yt', y_test)
-np.save('data', data)
-np.save('data_test', data_test)
-np.save('target', target)
-np.save('target_test', target_test)
-np.save('scale_params', scale_params)
+np.save(homeDir + 'binaryData/x.npy', x)
+np.save(homeDir + 'binaryData/y.npy', y)
+np.save(homeDir + 'binaryData/trData.npy', trainingData)
+np.save(homeDir + 'binaryData/teData.npy', testingData) 
+np.save(homeDir + 'binaryData/scale_params.npy', scale_params)
 print("Done.")
