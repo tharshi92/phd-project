2565ef9456c9a51189196299305c59b49ae606b2
Tharshi Srikannathasan <tharshisri@Tharshis-MacBook-Pro.local>
2016-11-24 13:01:54 -0500

    added prepMap.py for saving of surface information

---
+++ naAnalysis/prepMaps.py
@@ -0,0 +1,44 @@
+from config import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+dumpFolder = saveDir
+
+for metadata in metadatum:
+    
+    var_name = metadata[0]
+    title = metadata[1]
+    units = metadata[2]
+    
+    ghost_file = dumpFolder + title + '.npy'
+    if os.path.isfile(ghost_file):
+        print(ghost_file + ' already exists!! Skipping to next data stream.')
+        continue
+    
+    print('Preparing ' + title + ' Data..')
+    
+    for i in range(d):
+        print(fnames[i])
+        trFile = fnames[i]
+        data = Dataset(trFile)
+        
+        for j in range(24):
+            
+            idx = 24 * i + j
+            
+            if var_name == 'PBLDEPTH__PBL_M':
+                dataMap = data.variables[var_name][j, lat_i:lat_f, lon_i:lon_f]
+            else:
+                dataMap = data.variables[var_name][j, 0, lat_i:lat_f, lon_i:lon_f]
+            
+            rawData[idx, :, :] = dataMap
+        
+        data.close()
+
+    np.save(dumpFolder + title, rawData)
+    print('done.')
+
+
+
+
