c2aa3e92aed8dd306f8139f875d1df673d3b199c
Tharshi Srikannathasan <tharshisri@icloud.com>
2016-12-02 15:47:51 -0500

    minor update

--- animusCode/prepPBL.py
+++ animusCode/prepPBL.py
@@ -1,4 +1,4 @@
-from config import *
+from config import * 
 import sys
 
 folderName = sys.argv[1]
@@ -8,17 +8,12 @@ if not os.path.exists(dumpFolder):
 
 os.chdir(dumpFolder)
 
-metadata = pbl_metadata
-var_name = metadata[0]
-title = metadata[1]
-units = metadata[2]
-
-ghost_file = dumpFolder + title + '.npy'
+ghost_file = dumpFolder + 'PBL.npy'
 if os.path.isfile(ghost_file):
     print(ghost_file + ' already exists!! Goodbye.')
     sys.exit()
 
-print('Preparing ' + title + ' Data..')
+print('Preparing PBL Data..')
 
 for i in range(d):
 
@@ -27,12 +22,12 @@ for i in range(d):
 
     for j in range(24):
         idx = 24 * i + j
-        dataMap = data.variables[var_name][j, lat_i:lat_f, lon_i:lon_f]
+        dataMap = data.variables['PBLDEPTH__PBL_L'][j, lat_i:lat_f, lon_i:lon_f]
         mapMean = np.mean(dataMap, dtype=np.float64)
         rawData[idx, :] = mapMean
 
     data.close()
 
-np.save(dumpFolder + title, rawData)
+np.save(dumpFolder + 'PBL', rawData)
 print('done.')
 
