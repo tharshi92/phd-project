466b84a905de0ab8eae7a5d17eba1317cfe85b00
Tharshi Srikannathasan <tharshisri@icloud.com>
2016-12-02 14:14:55 -0500

    minor update

---
+++ animusCode/prepPBL.py
@@ -0,0 +1,45 @@
+from config import *
+import sys
+
+folderName = sys.argv[1]
+dumpFolder = homeDir + folderName + '/'
+if not os.path.exists(dumpFolder):
+    os.makedirs(dumpFolder)
+
+os.chdir(dumpFolder)
+
+levels = int(sys.argv[2])
+
+metadata = pbl_metadata
+var_name = metadata[0]
+title = metadata[1]
+units = metadata[2]
+
+ghost_file = dumpFolder + title + '.npy'
+if os.path.isfile(ghost_file):
+    print(ghost_file + ' already exists!! Skipping to next data stream.')
+    continue
+
+print('Preparing ' + title + ' Data..')
+
+for i in range(d):
+    
+    trFile = fnames[i]
+    data = Dataset(trFile)
+    
+    for j in range(24):
+        
+        idx = 24 * i + j        
+
+        dataMap = data.variables[var_name]\
+        		[j, lat_i:lat_f, lon_i:lon_f]
+            
+        mapMean = np.mean(dataMap, dtype=np.float64)
+        
+        rawData[idx, :] = mapMean
+    
+    data.close()
+
+np.save(dumpFolder + title, rawData)
+print('done.')
+
--- naAnalysis/makeMov.py
+++ naAnalysis/makeMov.py
@@ -6,7 +6,7 @@ from mpl_toolkits.basemap import Basemap, cm
 
 ''' This code will make a movie of a series of 2d plots. '''
 
-dataFolder = homeDir + 'surface/'
+dataFolder = homeDir + 'surfaceData/'
 rawData = np.load(dataFolder + 'COField.npy')
 
 fig = plt.figure()
@@ -30,14 +30,14 @@ def animate(i):
 	data = rawData[i, :, :]
 	m.imshow(data, cmap='Blues', interpolation='None', vmin=0, vmax=400)
 
-anim = animation.FuncAnimation(fig, animate, np.arange(0, len(rawData)), interval=60)
+anim = animation.FuncAnimation(fig, animate, np.arange(0, len(rawData)), interval=200)
 
 FFMpegWriter = animation.writers['ffmpeg']
 metadata = dict(title='Surface CO Field Data', \
                 artist='tsrikann@physics.utoronto.ca')
 writer = FFMpegWriter(fps=15, metadata=metadata)
 
-anim.save(dataFolder + 'COFieldMovie.mp4', writer=writer)
+#anim.save(dataFolder + 'COFieldMovie.mp4', writer=writer)
 
 plt.show()
 print('done.')
