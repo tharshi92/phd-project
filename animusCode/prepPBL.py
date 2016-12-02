ced5d0a6e04060b6ee9133fd2722523ae1009e52
Tharshi Srikannathasan <tharshisri@icloud.com>
2016-12-02 14:29:05 -0500

    minor update

--- animusCode/prepPBL.py
+++ animusCode/prepPBL.py
@@ -23,21 +23,16 @@ if os.path.isfile(ghost_file):
 print('Preparing ' + title + ' Data..')
 
 for i in range(d):
-    
+
     trFile = fnames[i]
     data = Dataset(trFile)
-    
-    for j in range(24):
-        
-        idx = 24 * i + j        
 
-        dataMap = data.variables[var_name]\
-        		[j, lat_i:lat_f, lon_i:lon_f]
-            
+    for j in range(24):
+        idx = 24 * i + j
+        dataMap = data.variables[var_name][j, lat_i:lat_f, lon_i:lon_f]
         mapMean = np.mean(dataMap, dtype=np.float64)
-        
         rawData[idx, :] = mapMean
-    
+
     data.close()
 
 np.save(dumpFolder + title, rawData)
