803e083f029e8dc5a44c5fe0a4e267e41775e204
Tharshi Srikannathasan <tharshisri@icloud.com>
2016-12-05 12:51:23 -0500

    minor update

--- naAnalysis/makeMov.py
+++ naAnalysis/makeMov.py
@@ -4,32 +4,40 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.basemap import Basemap, cm
 
-''' This code will make a movie of a series of 2d plots. '''
 
+# I/O
 dataFolder = homeDir + 'surfaceData/'
 rawData = np.load(dataFolder + 'COField.npy')
 
-fig = plt.figure()
+# Define Helper Functions
 
-m = Basemap(projection='mill', \
-			resolution='c', \
-			llcrnrlat=lats[lat_i], \
-			urcrnrlat=lats[lat_f], \
-            llcrnrlon=lons[lon_i], \
-            urcrnrlon=lons[lon_f])
+def generate_data
 
-m.drawcoastlines()
-m.drawcountries()
+def update(data):
+	mat.set_data(data)
+	return mat
+	data = rawData[i, :, :]
+	plt.imshow(data, cmap='Blues', interpolation='None', vmin=0, vmax=400)
+	plt.title(str(i))
+
+
+fig, ax = plt.subplots()
+
+# m = Basemap(projection='mill', \
+# 			resolution='c', \
+# 			llcrnrlat=lats[lat_i], \
+# 			urcrnrlat=lats[lat_f], \
+#             llcrnrlon=lons[lon_i], \
+#             urcrnrlon=lons[lon_f])
+
+# m.drawcoastlines()
+# m.drawcountries()
 
 data = rawData[0, :, :]
 
 p = m.imshow(data, cmap='Blues', interpolation='None', vmin=0, vmax=400)
 cb = m.colorbar(p, "right", size="5%", pad='2%')
 
-def animate(i):
-	data = rawData[i, :, :]
-	m.imshow(data, cmap='Blues', interpolation='None', vmin=0, vmax=400)
-	plt.title(str(i))
 
 anim = animation.FuncAnimation(fig, animate, np.arange(0, len(rawData)), interval=200)
 
