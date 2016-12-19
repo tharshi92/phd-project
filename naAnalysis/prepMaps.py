5698ad916d5894e0102f21b680a89491b16281d2
Tharshi Srikannathasan <tharshisri@icloud.com>
2016-12-19 16:06:54 -0500

    minor update

--- naAnalysis/prepMaps.py
+++ naAnalysis/prepMaps.py
@@ -1,40 +1,31 @@
 from configMaps import *
 from netCDF4 import Dataset
 
-for metadata in metadatum:
-    
-    var_name = metadata[0]
-    title = metadata[1]
-    units = metadata[2]
-    
-    ghost_file = saveDir + title + '.npy'
-    if os.path.isfile(ghost_file):
-        print(ghost_file + ' already exists!! Skipping to next data stream.')
-        continue
-    
-    print('Preparing ' + title + ' Data..')
-    
-    for i in range(d):
-        trFile = fnames[i]
-        data = Dataset(trFile)
-        
-        for j in range(24):
-            
-            idx = 24 * i + j
-            
-            if var_name == 'PBLDEPTH__PBL_M':
-                dataMap = data.variables[var_name]\
-                [j, latIndex1:latIndex2, lngIndex1:lngIndex2]
-            else:
-                dataMap = np.mean(data.variables[var_name]\
-                    [j, :18, latIndex1:latIndex2, lngIndex1:lngIndex2], axis=0)
-            
-            rawData[idx, :, :] = dataMap
-        
-        data.close()
+metadata = field_metadata
 
-    np.save(saveDir + title, rawData)
-    print('done.')
+var_name = metadata[0]
+title = metadata[1]
+units = metadata[2]
+
+print('Preparing ' + title + ' Data..')
+
+for i in range(d):
+    trFile = fnames[i]
+    data = Dataset(trFile)
+    
+    for j in range(24):
+        
+        idx = 24 * i + j
+
+        dataMap = np.mean(data.variables[var_name]\
+            [j, :18, latIndex1:latIndex2, lngIndex1:lngIndex2], axis=0)
+        
+        rawData[idx, :, :] = dataMap
+    
+    data.close()
+
+np.save(saveDir + title, rawData)
+print('done.')
 
 
 
