db87b6a9b1d61119791414cdd3ebe208530fd93f
Tharshi Srikannathasan <tharshisri@icloud.com>
2017-02-20 16:06:14 -0500

    updated coNotebook

--- source/coNotebook.py
+++ source/coNotebook.py
@@ -6,6 +6,7 @@
 # Import modules
 import numpy as np
 import tensorflow as tf
+
 import sys
 import os.path
 from time import time
@@ -15,19 +16,21 @@ import matplotlib.pyplot as plt
 get_ipython().magic(u'matplotlib inline')
 
 from config import *
-
 cwd = os.getcwd()
 
 # Start an interactive session
 sess = tf.InteractiveSession()
 
 
-# In[3]:
-
-local_repo = home_dir + "18level" + '/'
+# In[4]:
 
 # change to data directory
+local_repo = home_dir + "18level" + '/'
+
 os.chdir(local_repo)
+
+# Reset tf graph
+tf.reset_default_graph()
 
 # Load Training/Testing Data
 x = np.load('trData.npy')[:, :-1]
@@ -40,57 +43,46 @@ n_features = len(x.T)
 n_targets = len(y.T)
 
 
-# In[9]:
-
-# Create helper functions to build network
-
-def weights(size, n):
-    """Creates a weight tensor."""
-    return tf.Variable(tf.random_normal(size, stddev=1.0/size[0]), name='W' + str(n))
-
-def bias(size, n):
-    """Creates a bias tensor"""
-    return tf.Variable(tf.random_normal(size), name='b' + str(n))
-
-def relu(s):
-    """A lazy wrapper for the reulo function
-    in tensorflow."""
-    return tf.nn.relu(s)
-
-def ff_layer(s, size, n, linear=False):
-    """Take a signal s and return the activations of that layer."""
-    w = weights(size, n)
-    b = bias([size[0]], n)
-    if linear == False:
-        return relu(tf.matmul(s, w) + b)
-    else:
-        return tf.matmul(s, w) + b
-
-
-# In[10]:
+# In[5]:
 
 # Create placeholders for the input 
 # and the target
-v = tf.placeholder(tf.float32, shape=[None, n_features])
-z_ = tf.placeholder(tf.float32, shape=[None, n_targets])
+with tf.name_scope('input'):
+    x = tf.placeholder(tf.float32, shape=[None, n_features])
+    y_ = tf.placeholder(tf.float32, shape=[None, n_targets])
 
 
-# In[11]:
+# In[3]:
+
+# Create network helper functions
+
+def create_weights(input_dim, n_units, name):
+    """A helper function for creating weights."""
+    with tf.name_scope("Weights for Layer " + str(name)):
+        return tf.Variable(tf.random_normal(shape=[input_dim, n_units],nstddev=1.0/input_dim))
+    
+def create_biases(n_biases, name):
+    """A helper function for creating biases."""
+    with tf.name_scope("Biases for Layer " + str(name)):
+        return tf.Variable(tf.zeros(shape=[n_biases]))
+    
+def ff_layer(signal, n_units, num, act='relu'):
+    """Send a signal through a layer of neuron
+    units and returns the activations and weights if needed.
+    
+    """
+    
+    # Create weights and biases
+    W = create_weights(signal.shape[0], n_units, num)
+    b = create_biases(signal.shape[0], num)
+    
+    return tf.nn.relu(tf.matmul(signal, W) + b)
+
+
+# In[13]:
 
 # Initialize all tf variables
 sess.run(tf.global_variables_initializer())
-
-# This is our regression model
-a1 = ff_layer(v, [n_features, 5], 1)
-a2 = ff_layer(tmp, [5, 3], 2)
-z = ff_layer(a2, [3, n_targets], 3, linear=True)
-
-# Define a loss function
-loss = tf.reduce_mean(tf.nn.l2_loss(z - z_))
-
-
-# In[5]:
-
 z1 = z.eval(feed_dict={v: x_t})
 plt.figure(figsize=(18, 10))
 plt.plot(y_t, label='Test')
@@ -98,7 +90,7 @@ plt.plot(z1, label='Net')
 plt.legend()
 
 
-# In[6]:
+# In[ ]:
 
 # Train the model using gradient descent with a step size of 0.5
 train_step = tf.train.GradientDescentOptimizer(1e-2).minimize(loss)
@@ -108,7 +100,7 @@ for i in range(len(x)):
     train_step.run(feed_dict={v: x[i, :].reshape((1, 8)), z_: y[i, :].reshape((1, 1))})
 
 
-# In[7]:
+# In[ ]:
 
 z2 = z.eval(feed_dict={v: x_t})
 plt.figure(figsize=(18, 10))
@@ -117,22 +109,17 @@ plt.plot(z2, label='Net')
 plt.legend()
 
 
-# In[8]:
+# In[ ]:
 
 e1 = loss.eval(feed_dict={v: x, z_: y})
 
 
-# In[9]:
+# In[ ]:
 
 e2 = loss.eval(feed_dict={v: x_t, z_: y_t})
 
 
-# In[10]:
-
-print e1, e2
-
-
 # In[ ]:
 
-
+print e1, e2
 
