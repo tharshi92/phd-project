faffed1232a11c01607680a28499facf131b1bd1
Tharshi Srikannathasan <tharshisri@icloud.com>
2017-02-21 01:24:52 -0500

    added tensorboard viz to simple network model

--- source/coNotebook.py
+++ source/coNotebook.py
@@ -16,10 +16,6 @@ import matplotlib.pyplot as plt
 get_ipython().magic(u'matplotlib inline')
 
 from config import *
-cwd = os.getcwd()
-
-# Reset tf graph
-tf.reset_default_graph()
 
 # Start an interactive session
 sess = tf.InteractiveSession()
@@ -29,7 +25,6 @@ sess = tf.InteractiveSession()
 
 # change to data directory
 local_repo = home_dir + "18level" + '/'
-
 os.chdir(local_repo)
 
 # Load Training/Testing Data
@@ -39,6 +34,7 @@ y = np.load('trData.npy')[:, -1].reshape((len(x), 1))
 y_t = np.load('teData.npy')[:, -1].reshape((len(x_t), 1))
 scale_params = pickle.load(open('scaleParams.p', 'rb'))
 
+n = len(x)
 n_features = len(x.T)
 n_targets = len(y.T)
 
@@ -56,77 +52,100 @@ with tf.name_scope('input'):
 
 # Create network helper functions
 
-def create_weights(input_dim, n_units, name):
-    """A helper function for creating weights."""
-    with tf.name_scope("Weights_Layer_" + str(name)):
-        return tf.Variable(tf.random_normal(shape=[input_dim, n_units], 
-                                            stddev=1.0/float(input_dim)))
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+def variable_summaries(var):
+  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    #tf.summary.scalar('max', tf.reduce_max(var))
+    #tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
+
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
+  """It does a matrix multiply, bias add, and then uses relu to nonlinearize.
+  """
+  with tf.name_scope(layer_name):
+        
+    with tf.name_scope('weights'):
+      weights = weight_variable([input_dim, output_dim])
+      variable_summaries(weights)
+        
+    with tf.name_scope('biases'):
+      biases = bias_variable([output_dim])
+      variable_summaries(biases)
+        
+    with tf.name_scope('Wx_plus_b'):
+      preactivate = tf.matmul(input_tensor, weights) + biases
+      tf.summary.histogram('pre_activations', preactivate)
+        
+    activations = act(preactivate, name='activation')
+    tf.summary.histogram('activations', activations)
     
-def create_biases(n_biases, name):
-    """A helper function for creating biases."""
-    with tf.name_scope("Biases_Layer_" + str(name)):
-        return tf.Variable(tf.zeros(shape=[n_biases]))
-    
-def ff_layer(signal, n_units, num, act='relu'):
-    """Send a signal through a layer of neuron
-    units and returns the activations and weights if needed.
-    
-    """
-    with tf.name_scope("Layer_" + str(num)):
-        # Get size information
-        input_dim = signal.get_shape().as_list()[1]
-        # Create weights and biases
-        W = create_weights(input_dim, n_units, num)
-        b = create_biases(n_units, num)
-    
-        # Return activations
-        if act == 'relu':
-            return tf.nn.relu(tf.matmul(signal, W) + b)
-        else:
-            return tf.matmul(signal, W) + b
+    return activations
 
 
 # In[5]:
 
 # Use the previous functions to create a model
-fc1 = ff_layer(x_, 5, 1)
-fc2 = ff_layer(fc1, 3, 2)
-z = ff_layer(fc2, 1, 3)
+fc1 = nn_layer(x_, n_features, 5, 'layer1')
+fc2 = nn_layer(fc1, 5, 3, 'layer2')
+z = nn_layer(fc2, 3, 1, 'output', act=tf.identity)
 
 # Define the loss function
-with tf.name_scope("Loss"):
-    loss = tf.reduce_sum(tf.nn.l2_loss(z - y_))
-    
-# Initialize all tf variables
-sess.run(tf.global_variables_initializer())
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.nn.l2_loss(z - y_))
+tf.summary.scalar('loss', loss)
+
+with tf.name_scope('train'):
+  train_step = tf.train.AdamOptimizer(1e-2).minimize(loss)
 
 
 # In[6]:
 
-# Train the model using gradient descent with a step size of 0.5
-train_step = tf.train.GradientDescentOptimizer(1e-2).minimize(loss)
+# Merge all the summaries and write
+merged = tf.summary.merge_all()
+train_writer = tf.summary.FileWriter(save_dir + '/train',
+                                      sess.graph)
+test_writer = tf.summary.FileWriter(save_dir + '/test')
 
-# Run training step multiple times to reduce loss
-for i in range(len(x)):
-    train_step.run(feed_dict={x_: x[i, :].reshape((1, 8)), y_: y[i, :].reshape((1, 1))})
+# Initialize all tf variables
+sess.run(tf.global_variables_initializer())
+
+# Writer for logs
+writer = tf.summary.FileWriter(save_dir, graph=tf.get_default_graph())
+
+for i in range(200):
+  if i % 10 == 0:  # Record summaries and test-set accuracy
+    summary, err = sess.run([merged, loss], feed_dict={x_: x_t, y_: y_t})
+    test_writer.add_summary(summary, i)
+    print('Error at step %s: %s' % (i, err))
+  else:  # Record train set summaries, and train
+    summary, _ = sess.run([merged, train_step], feed_dict={x_: x, y_: y})
+    train_writer.add_summary(summary, i)
 
 
-# In[11]:
+# In[7]:
 
 z1 = z.eval(feed_dict={x_: x_t})
 plt.figure(figsize=(20, 10))
 plt.plot(y_t, label='Test')
 plt.plot(z1, label='Net')
 plt.legend()
-plt.savefig('fit1.png')
+plt.savefig(save_dir + '/fit1.png')
 
 
-# In[12]:
-
-mse = tf.nn.l2_loss(z1 - y_t)
+# In[ ]:
 
 
-# In[13]:
-
-print mse.eval()
 
